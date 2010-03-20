9d221ae78c97c7f2188e728f5f961bc9869c8ab9
Christoph <christoph@zerodeviation.net>
2010-03-20 15:00:51 -0500

    try and make it 2 dimensional.

--- project1/PARantenna.m
+++ project1/PARantenna.m
@@ -9,27 +9,30 @@ spacing = lambda/2;
 the_angle = 50*pi/180;
 theta_x = -the_angle:.01:the_angle;
 theta_y = -the_angle:.01:the_angle;
+[theta_x,theta_y] = meshgrid(0:2*pi/elements:2*pi,0:2*pi/elements:2*pi);
+% theta_y = theta_y';
 
 theta = atan(sqrt(tan(theta_x).^2+tan(theta_y).^2));
-phi = atan(tan(theta_x)/tan(theta_y));
+phi = atan(tan(theta_x)./tan(theta_y));
 
-dn =  zeros(3, elements);
+dn =  zeros(elements, elements);
 dn(1,1:elements) = spacing*[1:elements];
 
 k = 2*pi/lambda;
-a_r = [sin(theta)*cos(phi); sin(theta)*sin(phi);  cos(theta)];
-E = [];
+a_r = [sin(theta).*cos(phi); sin(theta).*sin(phi);  cos(theta)];
+% E = [];
 % loop through our angle
 for ii=1:length(theta_x)
 	tmp = 0;
-	
+
+	% get the location of where we want the E-field to be computed
+	a = a_r(:, ii) - a_0;	
+
 	% now loop through all the elements
 	for jj=1:elements
-		% get the location of where we want the E-field to be computed
-		a = a_r(:, ii) - a_0;
 		tmp = tmp + w(jj)*exp(j*k*dot(a,dn(:,jj)));
 	end
-	E = [E tmp];
+	E(ii) = tmp;
 end
 
 E_db = 10*log10((E.*conj(E)).^2);
