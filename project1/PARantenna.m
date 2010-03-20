65dd54272af0be9497b93991c5643482e2692ab1
Christoph <christoph@zerodeviation.net>
2010-03-20 13:14:50 -0500

    make part 1 work

--- project1/PARantenna.m
+++ project1/PARantenna.m
@@ -1,21 +1,39 @@
 
-% data points
-N = 100;
 elements = 25;
-lambda = .1
-spacing = lambda/2
-theta_x = [-50, 50]
-theta_y = [-50, 50]
+lambda = .1;
+a_0 = [0 0 0]';
+w = hann(elements);
+w = rectwin(elements);
 
+spacing = lambda/2;
+the_angle = 50*pi/180;
+theta_x = -the_angle:.01:the_angle;
+theta_y = -the_angle:.01:the_angle;
 
-theta_z = 0
-a_0 = 0;
-w = hanning(N);
+theta = atan(sqrt(tan(theta_x).^2+tan(theta_y).^2));
+phi = atan(tan(theta_x)/tan(theta_y));
 
+dn =  zeros(3, elements);
+dn(1,1:elements) = spacing*[1:elements];
 
-dn = [0:elements-1];
-dn = spacing*dn
 k = 2*pi/lambda;
-a_r = ax * theta_x + ay*theta_y + az* theta_z
+a_r = [sin(theta)*cos(phi); sin(theta)*sin(phi);  cos(theta)];
+E = [];
+% loop through our angle
+for ii=1:length(theta_x)
+	tmp = 0;
+	
+	% now loop through all the elements
+	for jj=1:elements
+		% get the location of where we want the E-field to be computed
+		a = a_r(:, ii) - a_0;
+		tmp = tmp + w(jj)*exp(j*k*dot(a,dn(:,jj)));
+	end
+	E = [E tmp];
+end
 
-E = sum(w*exp(j*k*dot(a_r-a_0, dn)))
+E_db = 10*log10((E.*conj(E)).^2);
+
+plot(E_db);
+xlabel('Angle (degrees)');
+ylabel('Antenna Pattern (dB)');
