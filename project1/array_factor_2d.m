a48505e7b26f008fbefb5120be7c071a06b65be5
Christoph <christoph@zerodeviation.net>
2010-04-21 09:26:56 -0500

    refactor stuff into a function

---
+++ project1/array_factor_2d.m
@@ -0,0 +1,25 @@
+function E = array_factor_2d(lambda, theta_x, theta_y, w, dn, a_0)
+	elements = length(dn);
+	theta = atan(sqrt(tan(theta_x).^2+tan(theta_y).^2));
+	phi = atan2(tan(theta_x),tan(theta_y));
+	k = 2*pi/lambda;
+	a_r = [sin(theta).*cos(phi); sin(theta).*sin(phi);  cos(theta)];
+	E = [];
+	% loop through our angle
+	for ii=1:length(theta_x)
+		tmp = 0;
+
+		% now loop through all the elements
+		for jj=1:elements
+			% get the location of where we want the E-field to be computed
+			a = a_r(:, ii) - a_0;
+			% a = repmat((a_r - a_0), N, 1);
+
+			tmp = tmp + w(jj)*exp(j*k*dot(a',dn(jj,:)));
+			% E = sum()
+		end
+		E = [E tmp];
+	end
+
+	E = 10*log10((E.*conj(E)));
+	E = E - max(E);
--- project1/prob2.m
+++ project1/prob2.m
@@ -10,42 +10,23 @@ a_0 = [0; 0; 1];
 w = rectwin(elements);
 
 % spacing = lambda/2;
-% spacing = .36/elements;
+spacing = .36/(elements-1);
 delta = .36/11.0;
-the_angle = 50*pi/180;
 
 % =========================
 % = no touchy beyond here =
 % =========================
 theta_x = linspace(-pi/2, pi/2, N);
 theta_y = linspace(-pi/2, pi/2, N);
-
-theta = atan(sqrt(tan(theta_x).^2+tan(theta_y).^2));
-phi = atan(tan(theta_x)/tan(theta_y));
+% theta_y = zeros(1,N);
 
 dn =  zeros(3, elements);
-dn(1,1:elements) = spacing*[0:elements-1];
-dn(1,1:elements) = delta.*[0 1 4 9 11];
-% dn = d';
+% dn(1,1:elements) = spacing*[0:elements-1];
+dn(1,1:elements) = delta*[0 1 4 9 11];
+dn = dn';
 
-k = 2*pi/lambda;
-a_r = [sin(theta)*cos(phi); sin(theta)*sin(phi);  cos(theta)];
-E = [];
-% loop through our angle
-for ii=1:length(theta_x)
-	tmp = 0;
-	
-	% now loop through all the elements
-	for jj=1:elements
-		% get the location of where we want the E-field to be computed
-		a = a_r(:, ii) - a_0;
-		tmp = tmp + w(jj)*exp(j*k*dot(a,dn(:,jj)));
-	end
-	E = [E tmp];
-end
+E_db = array_factor_2d(lambda, theta_x, theta_y, w, dn, a_0);
 
-E_db = 10*log10((E.*conj(E)).^2);
-E_db = E_db - max(E_db);
 title_str = sprintf('Azimuthal antenna pattern (%i elements, irregular spacing)', elements);
 % title_str = sprintf('Azimuthal antenna pattern (%i elements, %2.0f cm spacing)', elements, spacing*100);
 plot(theta_x*180/pi, E_db);
