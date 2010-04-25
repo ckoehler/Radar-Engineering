e08cac75a82b02bccfba7fa3d6e5423b2c81b8f9
Christoph <christoph@zerodeviation.net>
2010-04-24 19:18:11 -0500

    change problem 3 to use the dedicated function for the 3d array pattern.

--- project1/prob3.m
+++ project1/prob3.m
@@ -1,4 +1,4 @@
-open NWRT_ARRAY.mat;
+load NWRT_ARRAY.mat;
 d_x = d(:,1);
 d_y = d(:,2);
 plot(d_x, d_y, '*');
@@ -21,31 +21,7 @@ theta_x = linspace(-pi/2, pi/2, N);
 theta_y = linspace(-pi/2, pi/2, N);
 
 
-dn = d;
+E_db = array_factor_3d(lambda, theta_x, theta_x, w, d, a_0);
 
-k = 2*pi/lambda;
 
-% E = [];
-E = zeros(length(theta_x), length(theta_y));
-for ii=1:length(theta_x)
-	for kk=1:length(theta_y)
-		tmp = 0;
-
-		% get the location of where we want the E-field to be computed
-
-		theta = atan(sqrt(tan(theta_x(ii)).^2+tan(theta_y(kk)).^2));
-		phi = atan(tan(theta_y(ii))./tan(theta_x(kk)));
-		a_r = [sin(theta).*cos(phi); sin(theta).*sin(phi);  cos(theta)];
-		a = a_r - a_0;	
-		% now loop through all the elements
-		for jj=1:elements
-			tmp = tmp + w(jj)*exp(1i*k*dot(a,dn(jj,:)));
-		end
-		E(ii,kk) = tmp;
-		
-	end
-end
-
-E_db = 10*log10((E.*conj(E)).^2);
-% E_db1 = E_db - max(E_db);
 surf(E_db);
